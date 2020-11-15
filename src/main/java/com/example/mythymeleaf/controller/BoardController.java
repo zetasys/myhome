23da1e382bce3c44dc36aa3d78693a7d643d0fc7
GodCoder <zetasys21@gmail.com>
2020-11-15 13:15:22 +0900

    init

--- src/main/java/com/example/mythymeleaf/controller/BoardController.java
+++ src/main/java/com/example/mythymeleaf/controller/BoardController.java
@@ -1,14 +1,26 @@
 package com.example.mythymeleaf.controller;
 
+import com.example.mythymeleaf.model.Board;
+import com.example.mythymeleaf.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
     @GetMapping("/list")
-    public String list() {
+    public String list(Model model) {
+        List<Board> boards = boardRepository.findAll();
+        model.addAttribute("boards", boards);
         return "board/list";
     }
 }
